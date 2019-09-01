0d5878fad4288cc9739b0ca0449f8260d49ebb95
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-01 19:18:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,16 @@
-# class Song 
+class Song 
   
-# attr_accessor :name, :artist, :genre
-#   @@count = 0
-#   @@artists = []
-#   @@genres = []
+attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
   
-# def initialize(song_name,artist,genre)
-#   @name = song_name
-#   @artist = artist
-#   @genre = genre 
-#   @@count += 1 
-#   @@artists << artist 
-#   @@genres << genre
-# end
-# def
+def initialize(song_name,artist,genre)
+  @name = song_name
+  @artist = artist
+  @genre = genre 
+  @@count += 1 
+  @@artists << artist 
+  @@genres << genre
+end
+def
