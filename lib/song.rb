f8e0b471d5c7224d9e08965891c05ede5f72ae2f
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-01 19:13:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   @@artist = []
   @@genre = []
   
-  def initialize(song_name,artist,genre)
+ def initialize(song_name,artist,genre)
   @name = song_name
   @artist = artist
   @genre = genre 
